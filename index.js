6b1730645069cdb6a700ed216653ac001af81109
Tarmstrong95 <triston95strong@gmail.com>
2020-06-17 12:29:05 -0500

    Add functionality for rooms

--- index.js
+++ index.js
@@ -1,50 +1,68 @@
 const server = require('express')()
 const http = require('http').createServer(server)
 const io = require('socket.io')(http)
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
+const cors = require('cors')
+const { Socket } = require('dgram')
 
-let clients = 0
+server.use(cors())
 
-server.get('/', (req, res) => res.send('Cant get, you dumb shit!'))
+// ROOM STORAGE
+let ROOMS = {}
 
-io.sockets.setMaxListeners(11)
+server.get('/:id', (req, res) => {
+    let {id} = req.params
 
-io.on('connection', socket => {
-
-    socket.on('message', (message) => {
-        console.log('message received: ', message)
-        socket.broadcast.emit('message', message)
-    })
-
-    socket.on('NewClient', () => {
-        if(clients < 2){
-            socket.broadcast.emit('CreatePeer')
-            clients++
-            console.log(`New client established @ client: ${clients}`)
-        }else{
-            socket.emit('SessionActive')
-        }
-    })
-
-    socket.on('Offer', SendOffer)
-    socket.on('Answer', SendAnswer)
-    socket.on('disconnect', Disconnect)
+    // check if room exists
+    if(!ROOMS.id){
+        ROOMS[id] = createRoom(id)
+    }
+    res.status(201)
 })
 
-function Disconnect(){
-    if(clients>0) clients--
-    this.broadcast.emit('Disconnect')
+function createRoom(id){
+    let room = io
+    .of(id)
+    .on('connection', socket => {
+        console.log('connected')
+        socket.clients = 0
+
+        socket.on('message', (message) => {
+            console.log('message received: ', message)
+            socket.broadcast.emit('message', message)
+        })
+
+        socket.on('NewClient', () => {
+            if (socket.clients < 2) {
+                socket.broadcast.emit('CreatePeer')
+                socket.clients = socket.clients + 1
+                console.log(`New client established @ client: ${socket.clients}`)
+            } else {
+                socket.emit('SessionActive')
+            }
+        })
+
+        socket.on('Offer', (offer) => SendOffer(socket, offer))
+        socket.on('Answer', (data) => SendAnswer(socket, data))
+        socket.on('disconnect', () => Disconnect(socket))
+    })
+    return room
+}
+
+function Disconnect(socket){
+    if(socket.clients>0) socket.clients-=1
+    socket.broadcast.emit('Disconnect')
     console.log('User disconnected')
 }
 
-function SendOffer(offer) {
+function SendOffer(socket, offer) {
     console.log('sending offer')
-    this.broadcast.emit('BackendOffer', offer)
+    socket.broadcast.emit('BackendOffer', offer)
 }
 
-function SendAnswer(data) {
+function SendAnswer(socket, data) {
     console.log('sending answer')
-    this.broadcast.emit('BackendAnswer', data)
+    socket.broadcast.emit('BackendAnswer', data)
 }
 
 http.listen(PORT, () => console.log('Up and running you bitch'))
